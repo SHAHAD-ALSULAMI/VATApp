e6640e63763c395224abad53355f715a40677fcb
SHAHAD <106035762+SHAHAD-ALSULAMI@users.noreply.github.com>
2022-08-08 13:13:10 +0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,23 +3,50 @@ package com.example.vatapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vatapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
-
-    //object
-    private lateinit var binding: ActivityMainBinding
-
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =ActivityMainBinding.inflate(layoutInflater)
-        val view=binding.root
-        setContentView(view)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root // reference layout file
+        setContentView(view) // link layout
 
-        binding.btnCalculateVat.setOnClickListener{
+        binding.btCalcVAT.setOnClickListener {
             calculateVat()
-        }
-    }// End onCreate
+        } // End setOnClickListener
+
+    } // End onCreate
+
     private fun calculateVat() {
-        //
-    }
-}//End main
+        val stringVatet = binding.editTxt.text.toString()
+        val cost = stringVatet.toDouble()
+
+        // Radio group
+        val selectedID = binding.rgVat.checkedRadioButtonId
+
+        val vatPercentage = when(selectedID){
+            R.id.rbVat10 -> 0.10
+            R.id.rbVat15 -> 0.10
+            else -> 0.10
+        } // End when
+
+        var vat = vatPercentage * cost
+        var total = cost + vat
+        // Switch value
+        val roundVat = binding.switchRoundUp.isChecked
+
+        if (roundVat){
+            total = kotlin.math.ceil(total)
+        }
+
+        // Total Formatting
+        val formatedTotal = NumberFormat.getCurrencyInstance().format(total)
+
+        binding.txtCostTotal.text = getString(R.string.total_amount, formatedTotal)
+
+    } // End calculatVat() function
+} // End Class
+
+   
